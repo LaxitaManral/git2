2838c372b246656d5a1a79e4d4d863d788747e81
Ubuntu <ubuntu@ip-172-31-95-185.ec2.internal>
2025-03-20 07:32:08 +0000

    added all files

---
+++ bc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#CREATING A BACKUP WITH ROTATION
+#CREATING A FUNCTION
+
+function display_usage {
+	echo "usage: $0 path of sourcedir"
+}
+
+
+#checking the dir is exits or not
+
+if [ $# -eq 0 ] || [ ! -d "$1" ]; then
+	echo " invalid dir path"
+	display_usage
+	exit 1
+fi
+  
+#STORING THE SOURCE DIR PATH IN $
+
+source_dir="$1"
+destination_dir="/home/ubuntu/git2"
+function create_backup {
+	timestamp=$(date '+%y-%m-%d-%H')
+	backup_dir="${source_dir}/backup_${timestamp}"
+	mkdir -p "$destination_dir/gitbackup"
+	echo "backup fot gitbackup is completed"
+}
+
+#ROTATION OF BACKUP
+
+
+
+
+
+
+
+
+
+ 
+
+
